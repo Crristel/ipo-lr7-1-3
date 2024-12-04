0153a2747d1045be7dfa514aaaa2b8ae520a0a30
Anastasia <abalyeva.a@gmail.com>
2024-12-04 20:46:03 +0300

    Update index.py

--- index.py
+++ index.py
@@ -1,5 +1,8 @@
 import json
+operation_count=0
 def num1(read_file):
+    global operation_count
+    operation_count+= 1
     for i in read_file:
         for key, value in i.items():
             if key == 'id':
@@ -19,6 +22,7 @@ def num1(read_file):
         print()  
 
 def num2(read_file):
+    global operation_count
     user_id=int(input("Введите поле записи,которую необходимо вывести:"))
     find=False
     for i in read_file:
@@ -40,7 +44,8 @@ def num2(read_file):
                 elif key=='sub_type_count':
                     print(f"Количество подвидов рыб:{value}")
                     print()
-                    find=True    
+                    find=True 
+                    operation_count+= 1
             break
     if not find:
         print()
@@ -48,6 +53,7 @@ def num2(read_file):
         print()
         
 def num3(read_file):
+        global operation_count
         find=False
         while True:
             id=input("Введите номер рыбы:")
@@ -78,7 +84,7 @@ def num3(read_file):
                     break
                 else:
                     print("Это должно быть число!")
-                  
+
 #создаём из полученных данных множество 
             new_fish={"id": id, "name":user_name, 
                "latin_name": user_Lname,
@@ -90,8 +96,10 @@ def num3(read_file):
             with open("fish.json", 'w', encoding='utf-8') as out_file:
                 json.dump(read_file, out_file,ensure_ascii=False,indent=2)
                 print("Рыба успешно добавлена!")
-                
+                operation_count+= 1
+            
 def num4(read_file):
+    global operation_count
     user_remove = int(input("Введите номер для удаления: "))
     find=False
     for i in read_file:
@@ -101,40 +109,44 @@ def num4(read_file):
                 json.dump(read_file, new_file, ensure_ascii = False, indent=4)
                 find=True
                 print("Рыба успешно удалена!")
+                operation_count+= 1
                 break
     if not find:
         print()
         print("Запись не найдена!")
         print()
-      
-with open("fish.json", "r", encoding="utf-8") as file:
-    read_file = json.load(file)
-count=0
 
-while True:
-    print("Меню".center(24, "~"))
-    print("1) Вывести все записи")
-    print("2) Вывести запись по полю")
-    print("3) Добавить запись")
-    print("4) Удалить запись по полю")
-    print("5) Выйти из программы")
+def main():
+    global operation_count
+    with open("fish.json", "r", encoding="utf-8") as file:
+        read_file = json.load(file)
 
-    user_number = int(input("Для выбора введите номер пункта: "))
+    while True:
+        while True:
+            print("Меню".center(24,"~"))
+            print("1) Вывести все записи","2) Вывести запись по полю", "3) Добавить запись", "4) Удалить запись по полю","5) Выйти из программы",sep = "\n")
+            user_number=input("Для выбора введите номер пункта:")#если введённое с клавиатуры число равно 1, то выполняем первый пункт 
+            if user_number.isdigit():
+                user_number= int(user_number)
+                break
+            else:
+                print("Это должно быть число!")
     
-    if user_number == 1:
-        num1(read_file)
-        count+=1
-    elif user_number==2:
-        num2(read_file)
-        count+=1
-    elif user_number==3:
-        num3(read_file)
-        count+=1
-    elif user_number==4:
-        num4(read_file)
-        count+=1
-    elif user_number == 5:
-        print(f"Количество выполненных операций:{count}")
-        break  
-    else:
-        print("ERROR! please try again!")
+        if user_number == 1:
+            num1(read_file)
+            
+        elif user_number==2:
+            num2(read_file)
+
+        elif user_number==3:
+            num3(read_file)
+
+        elif user_number==4:
+            num4(read_file)
+
+        elif user_number == 5:
+            print(f"Количество выполненных операций:",operation_count)
+            break  
+        else:
+            print("ERROR! please try again!")
+main()
